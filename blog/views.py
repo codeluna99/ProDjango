4ee384554c3c211c3d55f80fe63db70f68a19ba2
Luna <codeluna99@gmail.com>
2021-04-10 18:13:11 +0530

    added a simple dataBase. title issue not resloved

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'writer-one',
-        'title' : 'Blog-1',
-        'date' : '24th august,2020',
-        'content': 'Here will be the content of the first blog',
-    },
-    {
-        'author': 'writer-two',
-        'title' : 'Blog-2',
-        'date' : '25th august,2020',
-        'content': 'Here will be the content of the SECOND blog',
-    }
-]
+from .models import Post
 
 def home(request):
 
     context = {
-        'posts' : posts
+        'posts' : Post.objects.all() 
     }
 
     return render(request, 'blog/home.html', context)
